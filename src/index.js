916ddc8f2470fbdebdb9b2a98c86074c1e8f9866
Tishtanya Shaw <tish@tishtanyas-air.mynetworksettings.com>
2023-12-16 19:52:28 -0500

    Initialize react app, Added react-router-dom, and homepage layout

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+	<Router>
+		<React.StrictMode>
+			<Navbar />
+			<App />
+			<Footer />
+		</React.StrictMode>
+	</Router>
+);
