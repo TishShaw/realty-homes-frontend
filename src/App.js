657d7dea95ee4281d2555cf5d74c886a2c7e2b5a
Tishtanya Shaw <tish@tishtanyas-air.mynetworksettings.com>
2023-12-19 07:44:22 -0500

    Added contact page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
 	return (
 		<Routes>
 			<Route path='/' element={<HomePage />} />
 			<Route path='/about-us' element={<AboutPage />} />
+			<Route path='/contact-us' element={<ContactPage />} />
 		</Routes>
 	);
 }
