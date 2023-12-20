46239fb8fbcf3bc35e152ff20ec7ce114b32e7ae
Tishtanya Shaw <tish@tishtanyas-air.mynetworksettings.com>
2023-12-20 02:17:35 -0500

    Added blog page, blog details page, and news json object

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+import BlogPage from './pages/BlogPage';
+import BlogDetails from './components/BlogDetails';
 
 function App() {
 	return (
 		<Routes>
 			<Route path='/' element={<HomePage />} />
 			<Route path='/about-us' element={<AboutPage />} />
+			<Route path='/our-blog' element={<BlogPage />} />
+			<Route path='/our-blog/:id' element={<BlogDetails />} />
 			<Route path='/contact-us' element={<ContactPage />} />
 		</Routes>
 	);
